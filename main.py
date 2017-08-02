92f96f4469f0bb0a1d65b56b0adb1c44a3111394
robert renecker <sylvan.rrenecker10@gmail.com>
2017-08-01 20:27:02 -0600

    training

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ END_SENT = "SENT_END"
 vocabulary_size = 8000
 unknown_token = "UNKNOWN_TOKEN"
 
+def softmax(x):
+    xt = np.exp(x - np.max(x))
+    return xt / np.sum(xt)
 
 class report:
     def __init__(self):
@@ -42,11 +45,12 @@ class report:
         #DICT = [(WORD, COUNT) FOR ENUMERATES RETURN OF (COUNT, WORD)]
         word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
 
-        for i, sent in enumerate(self.token_sent):
-            self.token_sent[i] = [w if w in word_to_index else unknown_token for w in sent]
+        for i, sent in enumerate(self.token_word):
+            self.token_word[i] = [w if w in word_to_index else unknown_token for w in sent]
 
-        self.X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in self.token_sent])
-        self.y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in self.token_sent])
+        self.X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in self.token_word])
+        print(self.X_train[10])
+        self.y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in self.token_word])
 
 
 
@@ -66,6 +70,7 @@ class RNNNumpy:
     def forward_propagation(self, x):
         #T = The total number of time steps
         T = len(x)
+        print(T)
         s = np.zeros((T + 1, self.hidden_dim))
         s[-1] = np.zeros(self.hidden_dim)
 
@@ -73,17 +78,142 @@ class RNNNumpy:
 
         #FOR EACH TIME STEP
         for t in np.arange(T):
+            #indexing U by x[t]. This is the same as multiplying U with a one-hot vector...
             s[t] = np.tanh(self.U[:,x[t]] + self.W.dot(s[t-1]))
             o[t] = softmax(self.V.dot(s[t]))
         return [o,s]
 
+    def predict(self, x):
+        #Perform forward propagation & return index of highest score.
+        o, s = self.forward_propagation(x)
+        return np.argmax(o, axis=1)
 
+    def calculate_loss(self, x, y):
+        #initialize Loss
+        L = 0
+        #For each sentence...
+        #print(len(y))
+        for i in np.arange(len(y)):
+            (o, s) = self.forward_propagation(x[i])
+            #We only care about the 'correct predctions'
+            correct_word_predictions = o[np.arange(len(y[i])), y[i]]
+            #Add to the loss based on how off we were...
+            L += -1 * np.sum(np.log(correct_word_predictions))
+        N = np.sum((len(yi) for yi in y))
+        return L/N
+
+
+    def bptt(self, x, y):
+        T = len(y)
+        # Perform forward propagation
+        o, s = self.forward_propagation(x)
+        # We accumulate the gradients in these variables
+        dLdU = np.zeros(self.U.shape)
+        dLdV = np.zeros(self.V.shape)
+        dLdW = np.zeros(self.W.shape)
+        delta_o = o
+        delta_o[np.arange(len(y)), y] -= 1.
+        # For each output backwards...
+        for t in np.arange(T)[::-1]:
+            dLdV += np.outer(delta_o[t], s[t].T)
+            # Initial delta calculation
+            delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
+            # Backpropagation through time (for at most self.bptt_truncate steps)
+            for bptt_step in np.arange(max(0, t-self.bptt_truncate), t+1)[::-1]:
+                # print "Backpropagation step t=%d bptt step=%d " % (t, bptt_step)
+                dLdW += np.outer(delta_t, s[bptt_step-1])
+                dLdU[:,x[bptt_step]] += delta_t
+                # Update delta for next step
+                delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step-1] ** 2)
+        return [dLdU, dLdV, dLdW]
+
+
+    def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
+        bptt_gradients = self.bptt(x, y)
+        model_parameters = ['U', 'V', 'W']
+        # Gradient check for each parameter
+        for pidx, pname in enumerate(model_parameters):
+            parameter = operator.attrgetter(pname)(self)
+            #print("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+            it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+            while not it.finished:
+                ix = it.multi_index
+                original_value = parameter[ix]
+                # estmate: (f(x+h) - f(x-h))/(2*h)
+                parameter[ix] = original_value + h
+                gradplus = self.calculate_total_loss([x],[y])
+                parameter[ix] = original_value - h
+                gradminus = self.calculate_total_loss([x],[y])
+                estimated_gradient = (gradplus - gradminus)/(2*h)
+                parameter[ix] = original_value
+                backprop_gradient = bptt_gradients[pidx][ix]
+                # calculate The relative error: (|x - y|/(|x| + |y|))
+                relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+                # If the error is to large fail the gradient check
+                if relative_error > error_threshold:
+                    #print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
+                    #print "+h Loss: %f" % gradplus
+                    #print "-h Loss: %f" % gradminus
+                    #print "Estimated_gradient: %f" % estimated_gradient
+                    #print "Backpropagation gradient: %f" % backprop_gradient
+                    #print "Relative Error: %f" % relative_error
+                    return
+                it.iternext()
+            #print "Gradient check for parameter %s passed." % (pname)
+
+    def sgd_step(self, x, y, learning_rate):
+        dLdU, dLdV, dLdW = self.bptt(x, y)
+        self.U -= (learning_rate * dLdU)
+        self.V -= (learning_rate * dLdV)
+        self.W -= (learning_rate * dLdW)
+
+    def train_with_sgd(self, X_train, y_train, learning_rate=0.005, nepoch=100, evaluate_loss_after=5):
+        #TODO: Add momentum!
+        losses = []
+        ex_seen = 0
+        for epoch in range(nepoch):
+            if (epoch % evaluate_loss_after == 0):
+                loss = self.calculate_loss(X_train, y_train)
+                losses.append((ex_seen, loss))
+                #print "Loss after num examples seen = %d epoch = %d: %f" % (ex_seen, epoch, loss)
+                if (len(losses) > 1 and losses[-1][1] > losses[-2][1]):
+                    learning_rate = learning_rate * 0.5
+                    #print "Setting learning rate to: ", learning_rate
+                sys.stdout.flush()
+            for i in range(len(y_train)):
+                self.sgd_step(X_train[i], y_train[i], learning_rate)
+                ex_seen += 1
+    def generate_sentence(t, model):
+        new_sentence = [t.word_to_index[START]]
+        while not new_sentence[-1] == t.word_to_index[END]:
+            next_word_probs = model.forward_prop(new_sentence)
+        #print next_word_probs[0][-1]
+            sampled_word = t.word_to_index[unknown_token]
+            while sampled_word == t.word_to_index[unknown_token]:
+                samples = np.random.multinomial(5, next_word_probs[0][-1])
+                sampled_word = np.argmax(samples)
+            new_sentence.append(sampled_word)
+        sentence_str = [t.index_to_word[x] for x in new_sentence[1:-1]]
+        return sentence_str
 
 
 
 if __name__ == "__main__":
-    print("Working Fine")
     egg = report()
-
+    print("Report is done gathering training data")
+    np.random.seed(10)
     model = RNNNumpy(vocabulary_size)
     print("Model Working")
+
+    losses = model.train_with_sgd(egg.X_train[:11000], egg.y_train[:11000], nepoch=40, evaluate_loss_after=1)
+    joblib.dump(model, 'trained_model_update.pkl')
+    """m = joblib.load('trained_model_update.pkl')
+
+    st = ""
+    for s in generate_sentence(egg, m):
+        if (s in ['!', '#', ':', '@', '.', '\'']):
+            st += s
+        else:
+            st += s + " "
+    print(st)
+    """
---
+++ testing.py
@@ -0,0 +1,11 @@
+#testing
+import numpy as np
+
+
+y = [3,29,45,19,1]#[12,45,91,22,434,1]]
+o = [.2, .3, .5, .6 ,.9]
+
+print(o)
+
+o = [np.arange(len(y)),y]
+print(o)
